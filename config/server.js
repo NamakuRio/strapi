e4e9d07e2243cf14cb185724e6cbd24324c00054
root <root@vps.server.local>
2020-07-21 22:02:16 +0000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd591b714da8ab9fa0b195b97b796cb7c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd772a02e-ff29-47a1-be43-ad9769a93eb5'
+};
